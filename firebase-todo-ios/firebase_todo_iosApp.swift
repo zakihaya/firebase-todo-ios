3b4229a577f73520c4552bc156eb95f7793ffed6
hayazaki <toru.hayazaki@gmail.com>
2022-04-25 13:47:15 +0900

    Initial Commit

---
+++ firebase-todo-ios/firebase_todo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_todo_iosApp.swift
+//  firebase-todo-ios
+//
+//  Created by haayzaki on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct firebase_todo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ firebase-todo-iosUITests/firebase_todo_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  firebase_todo_iosUITestsLaunchTests.swift
+//  firebase-todo-iosUITests
+//
+//  Created by haayzaki on 2022/04/25.
+//
+
+import XCTest
+
+class firebase_todo_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
